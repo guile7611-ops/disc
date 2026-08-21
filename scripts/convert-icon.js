a458e0a66b4b890f3e72ade2567534ed552e1943
Guilherme <seuemail@gmail.com>
2026-08-20 22:20:04 -0300

    feat: icone oficial do Psyduck de fone de ouvido incorporado no projeto

--- scripts/convert-icon.js
+++ scripts/convert-icon.js
@@ -5,26 +5,27 @@ const pngToIcoModule = require('png-to-ico');
 const pngToIco = typeof pngToIcoModule === 'function' ? pngToIcoModule : pngToIcoModule.default;
 
 async function processIcon() {
-  const inputPath = path.join(__dirname, '../build/icon.png');
+  const inputPath = path.join(__dirname, '../build/2445b282c3986a19eb1ee749817f4578.jpg');
   const squarePngPath = path.join(__dirname, '../build/icon_square.png');
-  const icoPath = path.join(__dirname, '../build/icon.ico');
+  const buildIconPath = path.join(__dirname, '../build/icon.png');
   const publicIconPath = path.join(__dirname, '../public/icon.png');
+  const icoPath = path.join(__dirname, '../build/icon.ico');
 
-  // Redimensiona a imagem enviada para um quadrado perfeito 512x512 mantendo transparência
+  // Redimensiona a imagem do Psyduck de fone de ouvido para um quadrado perfeito 512x512
   await sharp(inputPath)
     .resize(512, 512, {
-      fit: 'contain',
-      background: { r: 0, g: 0, b: 0, alpha: 0 }
+      fit: 'cover'
     })
+    .png()
     .toFile(squarePngPath);
 
-  fs.copyFileSync(squarePngPath, inputPath);
+  fs.copyFileSync(squarePngPath, buildIconPath);
   fs.copyFileSync(squarePngPath, publicIconPath);
 
-  // Gera o arquivo icon.ico com suporte a múltiplas resoluções para o Windows
+  // Gera o arquivo icon.ico nativo do Windows com o Psyduck
   const buf = await pngToIco(squarePngPath);
   fs.writeFileSync(icoPath, buf);
-  console.log('Ícones icon.png e icon.ico quadrados (512x512) gerados com sucesso!');
+  console.log('Ícone do Psyduck com fone gerado com sucesso!');
 }
 
 processIcon().catch(console.error);
